97ae71f4ce8d2321127e4b0ad5160205154642c1
abiisaleh <abi.saleh.25@gmail.com>
2024-05-05 09:50:00 +0700

    fix

--- resources/views/components/radio-list.blade.php
+++ resources/views/components/radio-list.blade.php
@@ -1,8 +1,10 @@
 @php
     $collumn['sm'] = $col['sm'] ?? 1;
     $collumn['md'] = $col['md'] ?? 2;
+
+    $cols = 'grid-cols-' . $collumn['sm'] . ' md:grid-cols-' . $collumn['md'];
 @endphp
 
-<ul class="grid w-full gap-2 md:gap-6 grid-cols-{{ $collumn['sm'] }} md:grid-cols-{{ $collumn['md'] }} text-center">
+<ul class="grid w-full gap-2 md:gap-6 text-center {{ $cols }}">
     {{ $slot }}
 </ul>
