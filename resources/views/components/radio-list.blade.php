771f790a09ff9615f3c34b94967df1435d9ed450
abiisaleh <abi.saleh.25@gmail.com>
2024-05-05 08:36:47 +0700

    fix

--- resources/views/components/radio-list.blade.php
+++ resources/views/components/radio-list.blade.php
@@ -1,8 +1,3 @@
-@php
-    $collumn['sm'] = $col['sm'] ?? 1;
-    $collumn['md'] = $col['md'] ?? 2;
-@endphp
-
-<ul class="grid w-full gap-2 md:gap-6 grid-cols-{{ $collumn['sm'] }} md:grid-cols-{{ $collumn['md'] }} text-center">
-    {{ $slot }}
-</ul>
+<div>
+    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
+</div>
