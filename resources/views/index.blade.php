243a87eba55f82eb09cd8bf895ff974ad105d256
abiisaleh <abi.saleh.25@gmail.com>
2024-06-02 14:06:28 +0900

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,7 +17,7 @@
                         @svg('heroicon-s-arrow-right', ['class' => 'w-5 h-5 ml-2 -mr-1'])
                     </a>
                     <a href="{{ filament()->getLoginUrl() }}"
-                        class="btn-shadow btn-shadow-white transition-all ease-in-out block mt-2 w-full md:w-auto md:mt-0 md:inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-full hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
+                        class="btn-shadow btn-shadow-white transition-all ease-in-out block mt-2 w-full md:w-auto md:mt-0 md:inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-full bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
                         Masuk
                     </a>
                 </div>
