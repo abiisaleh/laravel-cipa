5a8edc296885e2bd8e3cc05b7a03e514fb03338b
abiisaleh <abi.saleh.25@gmail.com>
2024-07-25 07:16:22 +0900

    fix delete pesanan

--- app/Filament/Resources/PembayaranResource/Pages/EditPembayaran.php
+++ app/Filament/Resources/PembayaranResource/Pages/EditPembayaran.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\PembayaranResource\Pages;
 
 use App\Filament\Resources\PembayaranResource;
 use App\Models\Pembayaran;
+use App\Models\StokTabung;
 use Filament\Actions;
 use Filament\Notifications\Actions\Action;
 use Filament\Notifications\Notification;
@@ -18,15 +19,8 @@ class EditPembayaran extends EditRecord
         return [
             Actions\DeleteAction::make()
                 ->before(function (Pembayaran $record) {
-                    //kembalikan stok tiap pesanan
-                    foreach ($record->pesanan as $pesanan) {
-                        //cek stok selain refill
-                        if (!strpos($pesanan->nama, 'refill')) {
-                            $tabung = \App\Models\Tabung::find($pesanan->tabung->id);
-                            $tabung->stok += $pesanan->qty;
-                            $tabung->save();
-                        }
-                    }
+                    foreach ($record->pesanan as $pesanan)
+                        StokTabung::where('kode', $pesanan->kode_tabung)->update(['digunakan' => false]);
                 }),
         ];
     }
